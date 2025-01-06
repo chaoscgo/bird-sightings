aa6415455d97740709c2d3e7736dd074a58fec48
Beverly <chaoscgo@hotmail.com>
2025-01-06 13:02:34 -0600

    added bird model

---
+++ back-end/models/birds/bird.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+    {
+      text: {
+        type: String,
+        required: true
+      },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    },
+    { timestamps: true }
+  );
+
+const birdSchema = new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: date,
+        required: true,
+      },
+      notes: {
+        type: String,
+        required: false,
+      },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      comments: [commentSchema]
+    },
+    { timestamps: true }
+  );
+
+  const Bird = mongoose.model('Bird', birdSchema);
+
+  module.exports = Bird;
