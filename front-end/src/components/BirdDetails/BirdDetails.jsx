7510637bd4b27ace34da0dfc3b295daff00ce9fc
Beverly <chaoscgo@hotmail.com>
2025-01-07 11:35:53 -0600

    added ability to create a new comment

--- front-end/src/components/BirdDetails/BirdDetails.jsx
+++ front-end/src/components/BirdDetails/BirdDetails.jsx
@@ -1,11 +1,11 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import * as birdService from '../../services/birdService';
+import CommentForm from '../CommentForm/CommentForm';
 
 const BirdDetails = (props) => {
 
     const [bird, setBird] = useState(null);
-
     const { birdId } = useParams();
 
     useEffect(() => {
@@ -15,6 +15,11 @@ const BirdDetails = (props) => {
         };
         fetchBird();
       }, [birdId]);
+
+      const handleAddComment = async (commentFormData) => {
+        const newComment = await birdService.createComment(birdId, commentFormData);
+        setBird({ ...bird, comments: [...bird.comments, newComment] });
+      };
 
       if (!bird) return <main>Loading...</main>;
 
@@ -35,6 +40,7 @@ const BirdDetails = (props) => {
           </p>
           <section>
             <h2>Comments</h2>
+            <CommentForm handleAddComment={handleAddComment} />
 
             {!bird.comments.length && <p>There are no comments.</p>}
 
--- front-end/src/services/birdService.js
+++ front-end/src/services/birdService.js
@@ -37,5 +37,26 @@ const index = async () => {
       console.log(error);
     }
   };
+
+  const createComment = async (birdId, commentFormData) => {
+    try {
+      const res = await fetch(`${BASE_URL}/${birdId}/comments`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(commentFormData),
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   
-  export { index, show, create };
+  export {
+     index,
+     show, 
+     create, 
+     createComment
+   };
